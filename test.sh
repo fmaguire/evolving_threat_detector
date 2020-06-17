affec92c4e5f7b0fd976168ec365823b32ff2137
Finlay Maguire <finlaymaguire@gmail.com>
2020-06-17 12:09:20 -0300

    Update test script

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python etd.py -i test/data/test_toy_salmonella.fna -d test/data/references -x 0.5 --verbose --debug
+python etd.py run -i test/data/test_toy_salmonella.fna -d test/data/references -x 0.5 --verbose --debug
